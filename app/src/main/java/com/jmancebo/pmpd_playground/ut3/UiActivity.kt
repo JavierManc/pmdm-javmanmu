a8cb5786b7002a0643b37082ca1248b742de84bc
Javier_Man <javiermacemu@gmail.com>
2021-10-06 18:13:07 +0200

    Añadida la clase UiActivity y la vista view_vertical_linearlayout con tres imágenes en vertical

---
+++ app/src/main/java/com/jmancebo/pmpd_playground/ut3/UiActivity.kt
@@ -0,0 +1,12 @@
+package com.jmancebo.pmpd_playground.ut3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jmancebo.pmpd_playground.R
+
+class UiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ui)
+    }
+}
