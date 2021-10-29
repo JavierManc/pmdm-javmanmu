c663cda4ff7505d00f05186349d444f33f3f524c
Javier_Man <javiermacemu@gmail.com>
2021-10-29 19:28:48 +0200

    Subida clase TapaModel

---
+++ app/src/main/java/com/jmancebo/pmpd_playground/ut02/TapaModel.kt
@@ -0,0 +1,6 @@
+package com.jmancebo.pmpd_playground.ut02
+
+import com.jmancebo.pmpd_playground.R
+
+class TapaModel (val titleRestaurant: Int = R.string.rest_1, val foodText: Int = R.string.food, val deliverText: Int = R.string.deliver,
+                 val resDistance: Int = R.string.distance, val resTime: Int = R.string.time, val newRes: Int = R.string.new_res)
