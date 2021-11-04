395463835a20a37bcd0ee8791fb47d0183fca8de
Javier_Man <javiermacemu@gmail.com>
2021-11-04 17:27:45 +0100

    Creados métodos setUpView y render en la actividad

--- app/src/main/java/com/jmancebo/pmpd_playground/alerts/presentation/AlertActivity.kt
+++ app/src/main/java/com/jmancebo/pmpd_playground/alerts/presentation/AlertActivity.kt
@@ -2,11 +2,40 @@ package com.jmancebo.pmpd_playground.alerts.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.widget.AppCompatTextView
 import com.jmancebo.pmpd_playground.R
+import com.jmancebo.pmpd_playground.alerts.data.AlertDataRepository
+import com.jmancebo.pmpd_playground.alerts.domain.AlertModel
+import com.jmancebo.pmpd_playground.alerts.domain.AlertRepository
+import com.jmancebo.pmpd_playground.alerts.domain.GetAlertUseCase
 
 class AlertActivity : AppCompatActivity() {
+
+    private val alertModel: AlertViewModel = AlertViewModel(GetAlertUseCase(AlertDataRepository()))
+
+
+    lateinit var infoTitleText: AppCompatTextView
+    lateinit var infoDateText: AppCompatTextView
+    lateinit var infoBodyText: AppCompatTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alert)
+        setUpView()
+        render()
+    }
+
+    private fun setUpView() {
+        infoTitleText = findViewById(R.id.info_title_text)
+        infoDateText = findViewById(R.id.info_date_text)
+        infoBodyText = findViewById(R.id.info_body_text)
+    }
+
+    private fun render() {
+        val alert = alertModel.getAlert().first()
+        infoTitleText.text = alert.title
+        infoDateText.text = alert.datePublished
+        infoBodyText.text = alert.body
     }
 }
