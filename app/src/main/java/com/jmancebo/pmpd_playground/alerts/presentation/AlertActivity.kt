4b259d9345aa7ac4e277cab284cccdeff8a60d9a
Javier_Man <javiermacemu@gmail.com>
2021-11-04 16:13:35 +0100

    Comienzo actividad de alerta

---
+++ app/src/main/java/com/jmancebo/pmpd_playground/alerts/presentation/AlertActivity.kt
@@ -0,0 +1,12 @@
+package com.jmancebo.pmpd_playground.alerts.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jmancebo.pmpd_playground.R
+
+class AlertActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert)
+    }
+}
