5da903a685c921bbf9c71366d8c1ac541bd52ba6
Javier_Man <javiermacemu@gmail.com>
2021-11-04 17:29:40 +0100

    Creado AlertModel con las clases AlertModel y FileEntity

---
+++ app/src/main/java/com/jmancebo/pmpd_playground/alerts/domain/AlertModel.kt
@@ -0,0 +1,11 @@
+package com.jmancebo.pmpd_playground.alerts.domain
+
+data class AlertModel(
+    val id: String, val title: String,
+    val type: Int, val summary: String,
+    val datePublished: String,
+    val body: String, val source: String,
+    val files: List<FileEntity>
+)
+
+data class FileEntity(val name: String, val url: String)
