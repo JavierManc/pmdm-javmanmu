b03464bc13869e60f291d32b4e60e52fdd749719
Javier_Man <javiermacemu@gmail.com>
2021-11-10 16:51:13 +0100

    Añadida función getAlerts que devuelve un listado de alertas

--- app/src/main/java/com/jmancebo/pmpd_playground/alerts/data/AlertDataRepository.kt
+++ app/src/main/java/com/jmancebo/pmpd_playground/alerts/data/AlertDataRepository.kt
@@ -3,21 +3,11 @@ package com.jmancebo.pmpd_playground.alerts.data
 import com.jmancebo.pmpd_playground.alerts.domain.AlertModel
 import com.jmancebo.pmpd_playground.alerts.domain.AlertRepository
 
-class AlertDataRepository(): AlertRepository {
+class AlertDataRepository(private val alertRemoteSource: AlertRemoteSource) : AlertRepository {
 
     override fun getAlerts(): List<AlertModel> {
-        return mutableListOf(
-            AlertModel(
-                "1",
-                "Título del primer alertmodel",
-                1,
-                "Summary del primer alertmodel",
-                "04/11/2022",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet lorem a metus aliquam, faucibus fermentum massa condimentum. Vivamus erat tellus, rhoncus et iaculis sed, condimentum quis augue. Aliquam erat volutpat. Duis venenatis sit amet velit quis egestas. Sed non diam scelerisque, ullamcorper augue nec, pellentesque nulla. Suspendisse porttitor at sapien varius facilisis. In mollis augue id sem laoreet rhoncus. Pellentesque aliquet pretium risus vel maximus.",
-                "Source del primer alertmodel",
-                mutableListOf()
-            )
-        )
+        val models = alertRemoteSource.getAlerts()
+        return models
     }
 
 
