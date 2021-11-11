3785b3010280c6bc1fea43059362625bd405deeb
Javier_Man <javiermacemu@gmail.com>
2021-11-11 17:03:56 +0100

    Actualizado AlertViewModel para cambiar las vistas de tipo AlertModel a AlertViewState

--- app/src/main/java/com/jmancebo/pmpd_playground/alerts/presentation/AlertViewModel.kt
+++ app/src/main/java/com/jmancebo/pmpd_playground/alerts/presentation/AlertViewModel.kt
@@ -4,7 +4,16 @@ import androidx.lifecycle.ViewModel
 import com.jmancebo.pmpd_playground.alerts.domain.AlertModel
 import com.jmancebo.pmpd_playground.alerts.domain.GetAlertUseCase
 
-class AlertViewModel(private val getAlertUseCase: GetAlertUseCase): ViewModel() {
+class AlertViewModel(private val getAlertUseCase: GetAlertUseCase) : ViewModel() {
 
-    fun getAlerts(): List<AlertModel> = getAlertUseCase.execute()
+    fun getAlerts(): List<AlertViewState> {
+        val alerts = getAlertUseCase.execute()
+        return alerts.map { alertModel ->
+            AlertViewState(
+                alertModel.title,
+                alertModel.datePublished,
+                alertModel.body
+            )
+        }
+    }
 }
