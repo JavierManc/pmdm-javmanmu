2922a4033947b5447db95cab6120e7dc8b6b2bbd
Javier_Man <javiermacemu@gmail.com>
2021-12-02 14:29:16 +0100

    Actualizado fragmento de formulario y creado view model de este

--- app/src/main/java/com/jmancebo/pmpd_playground/ut02/ex06/presentation/Ut02Ex06FormFragment.kt
+++ app/src/main/java/com/jmancebo/pmpd_playground/ut02/ex06/presentation/Ut02Ex06FormFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
 import com.jmancebo.pmpd_playground.databinding.FragmentUt02Ex06FormBinding
+import com.jmancebo.pmpd_playground.ut02.ex06.data.SharedPrefLocalSource
+import com.jmancebo.pmpd_playground.ut02.ex06.domain.SavePlayerUseCase
+import com.jmancebo.pmpd_playground.ut02.ex06.serializer.GsonSerializer
 
 class Ut02Ex06FormFragment : Fragment() {
 
     lateinit var binding: FragmentUt02Ex06FormBinding
+
+    lateinit var viewModel: Ut02Ex06FormViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +28,64 @@ class Ut02Ex06FormFragment : Fragment() {
 
     companion object {
         fun createInstance() = Ut02Ex06FormFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentFormButton.setOnClickListener {
+            savePlayer()
+        }
+    }
+
+    private fun savePlayer() {
+        binding.fragmentFormButton.setOnClickListener {
+            val player = getPlayerParam()
+            viewModel.savePlayer(player)
+        }
+    }
+
+    private fun getPlayerParam(): SavePlayerUseCase.Param {
+        val positions = createPositionList()
+        return SavePlayerUseCase.Param(
+            binding.formNameEdit.toString(),
+            binding.formSurnameEdit.toString(),
+            binding.formComunitySelection.selectedItem.toString(),
+            if (binding.formGenderFemale.isChecked) {
+                "Female"
+            } else {
+                "Male"
+            },
+            positions[0],
+            if (positions.size >= 2) {
+                positions[1]
+            } else {
+                ""
+            },
+            if (positions.size >= 3) {
+                positions[2]
+            } else {
+                ""
+            }
+        )
+    }
+
+    private fun createPositionList(): List<String> {
+        val positionList: MutableList<String> = mutableListOf()
+
+        if (binding.formPositionGoalkeeper.isChecked) {
+            positionList.add("Portero")
+        }
+        if (binding.formPositionStriker.isChecked) {
+            positionList.add("Delantero")
+        }
+        if (binding.formPositionDefender.isChecked) {
+            positionList.add("Defensa")
+        }
+        if (binding.formPositionMidfield.isChecked) {
+            positionList.add("Mediocentro")
+        }
+
+        return positionList
     }
 
 
