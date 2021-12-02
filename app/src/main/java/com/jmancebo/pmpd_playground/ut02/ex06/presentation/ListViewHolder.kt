c3e4fb43d2df9e790d6baae3034313ae30d6e060
Javier_Man <javiermacemu@gmail.com>
2021-12-02 17:44:25 +0100

    Añadido apellido al crear las cartas de jugadores

--- app/src/main/java/com/jmancebo/pmpd_playground/ut02/ex06/presentation/ListViewHolder.kt
+++ app/src/main/java/com/jmancebo/pmpd_playground/ut02/ex06/presentation/ListViewHolder.kt
@@ -5,12 +5,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.jmancebo.pmpd_playground.databinding.AddedFormItemBinding
 import com.jmancebo.pmpd_playground.ut02.ex06.domain.SavePlayerUseCase
 
-class ListViewHolder(view: View) : RecyclerView.ViewHolder(view){
+class ListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val bind = AddedFormItemBinding.bind(view)
 
-    fun render(model: SavePlayerUseCase.Param){
-        bind.fragmentCardName.text = model.name
+    fun render(model: SavePlayerUseCase.Param) {
+        bind.fragmentCardName.text = model.name + " " + model.surname
         bind.fragmentCardGender.text = model.gender
         bind.fragmentCardComunity.text = model.comunity
         bind.fragmentCardPrimaryPosition.text = model.firstPosition
