362f4dbb22f640575870e1fe33e8bee413367a04
Javier_Man <javiermacemu@gmail.com>
2021-12-02 14:28:15 +0100

    Creada clase para guardar los modelos en un fichero xml

---
+++ app/src/main/java/com/jmancebo/pmpd_playground/ut02/ex06/data/SharedPrefLocalSource.kt
@@ -0,0 +1,28 @@
+package com.jmancebo.pmpd_playground.ut02.ex06.data
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.jmancebo.pmpd_playground.ut02.ex06.domain.SavePlayerUseCase
+import com.jmancebo.pmpd_playground.ut02.ex06.serializer.JsonSerializer
+
+class SharedPrefLocalSource(
+    activity: AppCompatActivity,
+    private val json: JsonSerializer
+) {
+    private val sharedpref = activity.getSharedPreferences(
+        "player_shared_pref_ls",
+        Context.MODE_PRIVATE
+    )
+
+    fun save(model: SavePlayerUseCase.Param) {
+        val edit = sharedpref.edit()
+        edit.putString(model.name, json.toJson(model, SavePlayerUseCase.Param::class.java))
+        edit.apply()
+    }
+
+    fun fetchAll(): List<SavePlayerUseCase.Param> {
+        return sharedpref.all.map { entity ->
+            json.fromJson(entity.value.toString(), SavePlayerUseCase.Param::class.java)
+        }
+    }
+}
